d461ac649aca67c5d9194afbb5e77e12a10828a8
Leszek Żebrowski <leszek-zebrowski@o2.pl>
2020-08-01 12:57:33 +0200

    Funkcje skrotu

---
+++ src/SHAChecksum.java
@@ -0,0 +1,25 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class SHAChecksum {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        if (args.length != 1) {
+            System.out.println("Podaj ciąg znaków do obliczenia funkcji skrótu.");
+            return;
+        }
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+        byte[] mdbytes = md.digest(args[0].getBytes());
+        StringBuffer hexBuffer = new StringBuffer();
+        String hex;
+        for (int i = 0; i < mdbytes.length; ++i) {
+            hex = Integer.toHexString(0xFF & mdbytes[i]);
+            if (hex.length() == 1) {
+                hexBuffer.append('0');
+            }
+            hexBuffer.append(hex);
+        }
+        System.out.println(hexBuffer.toString());
+    }
+}
+
